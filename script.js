d23fb99ee2156ae6125380d083c11b4996888a77
itskme <160423464+itskme@users.noreply.github.com>
2024-08-30 18:06:57 +0000

    Done html start JS

---
+++ script.js
@@ -0,0 +1,14 @@
+let intervalId;
+let timeLeft;
+
+document.getElementById('start-btn').addEventListener('click', startCountdown);
+document.getElementById('reset-btn').addEventListener('click', resetCountdown);
+
+function startCountdown() {
+    const hours = parseInt(document.getElementById('hours').value);
+    const minutes = parseInt(document.getElementById('minutes').value);
+    const seconds = parseInt(document.getElementById('seconds').value);
+
+   
+}
+
